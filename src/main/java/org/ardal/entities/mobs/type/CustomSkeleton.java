3c9c67bfa12e2c0016f533caa35af13fd546790c
The_Gost_sniper <54660359+TheGostsniperfr@users.noreply.github.com>
2024-05-03 10:31:17 +0200

    ✅ Add mob random armors

--- src/main/java/org/ardal/entities/mobs/type/CustomSkeleton.java
+++ src/main/java/org/ardal/entities/mobs/type/CustomSkeleton.java
@@ -5,11 +5,13 @@ import org.ardal.utils.MathUtils;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Skeleton;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -51,19 +53,88 @@ public class CustomSkeleton extends CustomMob {
                 Mob power
          */
 
-        skeleton.addPotionEffect(new PotionEffect(PotionEffectType.ABSORPTION, Integer.MAX_VALUE, this.getMobLevel(), true, true));
+        skeleton.addPotionEffect(new PotionEffect(PotionEffectType.ABSORPTION, Integer.MAX_VALUE, this.getMobLevel(), true, false));
 
 
         if(this.getMobLevel() <= 30) {
-            skeleton.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, Integer.MAX_VALUE, this.getMobLevel() / 10, true, true));
+            skeleton.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, Integer.MAX_VALUE, this.getMobLevel() / 10, true, false));
         } else {
-            skeleton.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, Integer.MAX_VALUE, this.getMobLevel() / 5, true, true));
-            skeleton.addPotionEffect(new PotionEffect(PotionEffectType.FIRE_RESISTANCE, Integer.MAX_VALUE, 1, true, true));
+            skeleton.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, Integer.MAX_VALUE, this.getMobLevel() / 5, true, false));
+            skeleton.addPotionEffect(new PotionEffect(PotionEffectType.FIRE_RESISTANCE, Integer.MAX_VALUE, 1, true, false));
 
         }
 
         /*
                 Mob stuff
          */
+
+        List<ItemStack> possibleHelmet = new ArrayList<>();
+        List<ItemStack> possibleChestPlate = new ArrayList<>();
+        List<ItemStack> possibleLeggings = new ArrayList<>();
+        List<ItemStack> possibleBoots = new ArrayList<>();
+
+        List<Enchantment> possibleEnchantments = new ArrayList<>();
+
+        if (this.getMobLevel() <= 3) {
+            possibleHelmet.add(new ItemStack(Material.LEATHER_HELMET));
+            possibleChestPlate.add(new ItemStack(Material.LEATHER_CHESTPLATE));
+            possibleLeggings.add(new ItemStack(Material.LEATHER_LEGGINGS));
+            possibleBoots.add(new ItemStack(Material.LEATHER_BOOTS));
+        } else if (this.getMobLevel() <= 10) {
+            possibleHelmet.add(new ItemStack(Material.IRON_HELMET));
+            possibleChestPlate.add(new ItemStack(Material.IRON_CHESTPLATE));
+            possibleLeggings.add(new ItemStack(Material.IRON_LEGGINGS));
+            possibleBoots.add(new ItemStack(Material.IRON_BOOTS));
+
+            possibleEnchantments.add(Enchantment.PROTECTION_ENVIRONMENTAL);
+            possibleEnchantments.add(Enchantment.PROTECTION_FIRE);
+        } else if (this.getMobLevel() <= 20){
+            possibleHelmet.add(new ItemStack(Material.GOLDEN_HELMET));
+            possibleChestPlate.add(new ItemStack(Material.GOLDEN_CHESTPLATE));
+            possibleLeggings.add(new ItemStack(Material.GOLDEN_LEGGINGS));
+            possibleBoots.add(new ItemStack(Material.GOLDEN_BOOTS));
+
+            possibleEnchantments.add(Enchantment.PROTECTION_ENVIRONMENTAL);
+            possibleEnchantments.add(Enchantment.PROTECTION_FIRE);
+            possibleEnchantments.add(Enchantment.THORNS);
+        }
+
+        ItemStack helmet = getRandomItem(possibleHelmet, possibleEnchantments, 0.6);
+        ItemStack chestplate = getRandomItem(possibleChestPlate, possibleEnchantments, 0.3);
+        ItemStack leggings = getRandomItem(possibleLeggings, possibleEnchantments, 0.4);
+        ItemStack boots = getRandomItem(possibleBoots, possibleEnchantments, 0.6);
+
+        if (helmet != null) {
+            skeleton.getEquipment().setHelmet(helmet);
+        }
+
+        if (chestplate != null) {
+            skeleton.getEquipment().setChestplate(chestplate);
+        }
+
+        if (leggings != null) {
+            skeleton.getEquipment().setLeggings(leggings);
+        }
+
+        if (boots != null) {
+            skeleton.getEquipment().setBoots(boots);
+        }
+    }
+
+    @Nullable
+    private ItemStack getRandomItem(List<ItemStack> items, List<Enchantment> enchantments, double probability) {
+        if(MathUtils.getRandom(0, 100) < probability * 100) {
+            return setRandomEnchant(items.get(MathUtils.getRandom(0, items.size())), enchantments);
+        }
+
+        return null;
+    }
+
+    private ItemStack setRandomEnchant(ItemStack item, List<Enchantment> enchantments) {
+        for(Enchantment enchantment : enchantments){
+            item.addUnsafeEnchantment(enchantment, MathUtils.getRandom(1, enchantment.getMaxLevel()));
+        }
+
+        return item;
     }
 }
